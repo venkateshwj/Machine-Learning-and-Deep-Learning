25e6edd95b33fea0300468d8f879f93f027f7e66
venkateshwj <venkat@masha.io>
2019-02-05 06:57:42 +0000

    DL
    Signed-off-by: venkateshwj <venkat@masha.io>

---
+++ dl-learnings/CNN/cnn_traditional.py
@@ -0,0 +1,27 @@
+# CNN
+# Image -> Input Layer > Hidden Layer - Output Layer
+# 
+# Input Layer :
+#  Accepts the images as part of pixels and form arrays
+#
+# Hidde Layer
+# Feature Extraction
+#    - Convolution Layer (சுழற்சி)
+#    - Relu layer
+#    - Pooling layer 
+#         CL        RL        PL
+#   IL      O        O         O
+#   O       O        O         O
+#   O       O        O         O
+#   O       O        O         O
+#   O       O        O         O
+#
+#
+#  Images will be converted as matrix (Assume white space as 0
+#                                          and dark place as 1) 
+# 
+#  a= [5,3,2,5,9,7]
+#  b= [1,2,3]
+#  a * b = [5*1, 3*2, 2*3 ] = Sum = 17
+#          [5, 6, 6 ] = Sum = 17
+# 
